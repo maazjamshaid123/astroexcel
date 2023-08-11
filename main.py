01d1c601ae3c968b8db1778b2a4808c5fb058161
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-08-11 10:39:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
+import openpyxl
 import ephem
 from datetime import datetime
 import pytz
-from io import BytesIO
 
 # Function to calculate azimuth and elevation angles
 def calculate_angles(latitude, longitude, date, time):
@@ -21,58 +20,70 @@ def calculate_angles(latitude, longitude, date, time):
     observer.lat = str(latitude)
     observer.long = str(longitude)
 
-    # Create a new DataFrame to store results
-    results = pd.DataFrame(columns=['Object', 'Azimuth', 'Elevation'])
+    # Open the Excel file
+    workbook = openpyxl.load_workbook('celestial_angles.xlsx')
+    sheet = workbook.active
+
+    # Write user-provided input to Excel
+    sheet['A12'] = 'Latitude (degrees)'
+    sheet['A13'] = 'Longitude (degrees)'
+    sheet['A14'] = 'Date'
+    sheet['A15'] = 'Time (IST)'
+    sheet['B12'] = latitude
+    sheet['B13'] = longitude
+    sheet['B14'] = date_time_ist.strftime('%Y-%m-%d %H:%M')
+    sheet['B15'] = date_time_ist.strftime('%H:%M')
 
     # Calculate and write the angles for each object
     celestial_objects = ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'North Node', 'South Node']
-    for obj_name in celestial_objects:
+    for i, obj_name in enumerate(celestial_objects):
         if obj_name == 'North Node':
             obj_north = getattr(ephem, 'Moon')(observer)
             obj_north.compute(observer)
-            azimuth_north = obj_north.az * 180.0 / ephem.pi
-            elevation_north = obj_north.alt * 180.0 / ephem.pi
-            results = results.append({'Object': 'Node (Asc)', 'Azimuth': azimuth_north, 'Elevation': elevation_north}, ignore_index=True)
+            sheet.cell(i+2, 1, 'Node (Asc)')
+            # Get azimuth and elevation in degrees
+            az_north = obj_north.az * 180.0 / ephem.pi
+            alt_north = obj_north.alt * 180.0 / ephem.pi
+            sheet.cell(i+2, 2, az_north)
+            sheet.cell(i+2, 3, alt_north)
         elif obj_name == 'South Node':
             # Calculate South Node (Descending Lunar Node) based on North Node position
             obj_south = getattr(ephem, 'Moon')(observer)
             obj_south.compute(observer)
-            azimuth_north = obj_north.az * 180.0 / ephem.pi
-            elevation_north = obj_north.alt * 180.0 / ephem.pi
-            azimuth_south = (azimuth_north + 180.0) % 360.0
-            elevation_south = elevation_north * -1.0
-            results = results.append({'Object': 'Node (Dec)', 'Azimuth': azimuth_south, 'Elevation': elevation_south}, ignore_index=True)
+            az_north = obj_north.az * 180.0 / ephem.pi
+            alt_north = obj_north.alt * 180.0 / ephem.pi
+            az_south = (az_north + 180.0) % 360.0
+            alt_south = alt_north * -1.0
+            sheet.cell(i+2, 1, 'Node (Dec)')
+            sheet.cell(i+2, 2, az_south)
+            sheet.cell(i+2, 3, alt_south)
         else:
             obj = getattr(ephem, obj_name)()
             obj.compute(observer)
-            azimuth = obj.az * 180.0 / ephem.pi
-            elevation = obj.alt * 180.0 / ephem.pi
-            results = results.append({'Object': obj_name, 'Azimuth': azimuth, 'Elevation': elevation}, ignore_index=True)
+            sheet.cell(i+2, 1, obj_name)
+            # Get azimuth and elevation in degrees
+            az = obj.az * 180.0 / ephem.pi
+            alt = obj.alt * 180.0 / ephem.pi
+            sheet.cell(i+2, 2, az)
+            sheet.cell(i+2, 3, alt)
 
-    # Save the results to an Excel file
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        results.to_excel(writer, index=False, sheet_name='Celestial Angles')
-    output.seek(0)
-    return output
+    # Save the Excel file
+    workbook.save('celestial_angles.xlsx')
 
 # Streamlit UI
 st.title('Celestial Object Angles Calculator')
-st.write('Enter your location and date/time (IST) to calculate azimuth and elevation angles.')
 
-latitude = st.number_input('Latitude (degrees):', -90.0, 90.0, 35.6895)
-longitude = st.number_input('Longitude (degrees):', -180.0, 180.0, 139.6917)
-date = st.date_input('Date (IST):')
-time = st.time_input('Time (IST):')
+# User input
+latitude = st.text_input('Enter Latitude (degrees):', '35.6895')
+longitude = st.text_input('Enter Longitude (degrees):', '139.6917')
+date = st.date_input('Enter Date:')
+time = st.time_input('Enter Time (IST):')
 
+# Calculate angles
 if st.button('Calculate Angles'):
-    # Call the function to calculate angles and generate Excel file
-    excel_file = calculate_angles(latitude, longitude, date.strftime('%Y-%m-%d'), time.strftime('%H:%M'))
-    st.success('Angles calculated! You can download the Excel file below.')
-    st.download_button('Download Excel File', excel_file, file_name='celestial_angles.xlsx', key='excel-download')
-
-# Display the calculated results in a DataFrame
-st.subheader('Calculated Angles:')
-if 'excel_file' in locals():
-    df = pd.read_excel(excel_file)
-    st.write(df)
+    try:
+        # Call the function to calculate angles and update the Excel file
+        calculate_angles(float(latitude), float(longitude), date.strftime('%Y-%m-%d'), time.strftime('%H:%M'))
+        st.success('Angles calculated and saved in celestial_angles.xlsx!')
+    except Exception as e:
+        st.error(f'An error occurred: {e}')
